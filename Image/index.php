d07976529a47ae1a297ff36dfc7941b5f26e432a
Sri-Maradani <maruthimaradani@gmail.com>
2020-05-20 13:47:57 +0530

    uploading an image in php

---
+++ Image/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>
+        Image upload
+        <link rel="stylesheet" type="text/css" href="style.css">
+        
+    </title>
+</head>
+<body class="sillu">
+    <p>Maruthi Creations</p>
+    <form action="upload.php" method="POST" enctype="multipart/form-data">
+        <input type="file" name="file">
+        <section class="siii">
+            <button type="submit" name="submit">UPLOAD FILE</button>
+    </section>
+    </form>
+</body>
+</html>
